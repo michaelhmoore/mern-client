d99dd1df09b8480753ff08201ae2ca9bc7fbe0f9
michael moore <michael.moore@hey.com>
2024-05-12 21:20:28 -0400

    Create MessageList component

---
+++ src/components/MessageList.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
+import Message from './Message';
+import { getMessages, deleteMessage } from '../MessageApi'
+
+function MessageList() {
+  const [messages, setMessages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  function deleteFromList(messageId) {
+    deleteMessage(messageId);
+
+    setMessages(prevMessages => prevMessages.filter(
+      message => message._id !== messageId));
+  }
+
+  useEffect(() => {
+    async function getAllMessages() {
+      const messages = await getMessages();
+      setMessages(messages);
+      setIsLoading(false);
+    }
+
+    getAllMessages();
+  }, []);
+
+  return (
+    <>
+      {isLoading
+        ? (
+          <Spinner animation='border' role='status'>
+            <span className='visually-hidden'>Loading...</span>
+          </Spinner>
+        ) : messages.map(message => (
+          <Message key={message._id} value={message} delete={deleteFromList} />
+        ))
+      }
+    </>
+  );
+}
+
+export default MessageList;
